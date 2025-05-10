e15b7c2f579346ac831af700d265f5c79102a679
Amarjeet Kumar <bidupur8544@gmail.com.com>
2025-05-10 18:06:24 +0530

    some change

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
   constructor(
     private _router: Router
   ) {
 
+  }
+  ngOnInit(): void {
+    AOS.init({
+      duration: 2000,
+      once: true
+    });
   }
   industryOverview(): boolean {
     return this._router.isActive('/industry-overview', true);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,9 @@ import * as AOS from 'aos';
 })
 export class HomeComponent {
   @ViewChild('bgVideo') bgVideo!: ElementRef<HTMLVideoElement>;
-
+// Group testimonials by 3 per slide
+  groupedTestimonials: any = [];
+  
   ngAfterViewInit() {
     const videoEl = this.bgVideo.nativeElement;
 
@@ -19,10 +21,25 @@ export class HomeComponent {
   }
 
   ngOnInit(): void {
+    const groupSize = 3;
+    for (let i = 0; i < this.testimonials.length; i += groupSize) {
+      this.groupedTestimonials.push(this.testimonials.slice(i, i + groupSize));
+    }
     AOS.init({
       duration: 2000,
       once: true
     });
   }
+
+
+  testimonials = [
+    { name: 'John Doe', role: 'Engineer', message: 'Awesome!', avatar: '../../assets/img/about.png' },
+    { name: 'Jane Smith', role: 'Designer', message: 'Loved it!', avatar: '../../assets/img/about.png' },
+    { name: 'Mike Lee', role: 'PM', message: 'Very useful.', avatar: '../../assets/img/about.png' },
+    { name: 'Emily Ray', role: 'QA', message: 'Bug-free!', avatar: '../../assets/img/about.png' },
+    { name: 'Chris Green', role: 'CTO', message: 'Superb!', avatar: '../../assets/img/about.png' },
+    { name: 'Sara Kim', role: 'Data Scientist', message: 'Great insights.', avatar: '../../assets/img/about.png' }
+  ];  
+
 
 }
